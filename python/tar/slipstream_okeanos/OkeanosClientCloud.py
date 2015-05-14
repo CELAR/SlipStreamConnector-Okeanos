d1dc5435c76426558859b9b0e91ed1df00bd2ac7
Christos KK Loverdos <loverdos@gmail.com>
2015-05-14 13:31:11 +0300

    Generate an RSA keypair in the bootstrap_script

--- python/tar/slipstream_okeanos/OkeanosClientCloud.py
+++ python/tar/slipstream_okeanos/OkeanosClientCloud.py
@@ -473,8 +473,9 @@ class OkeanosClientCloud(BaseCloudConnector):
 
     def _get_bootstrap_script(self, node_instance, pre_bootstrap=None,
                               post_bootstrap=None, username=None, **kwargs):
-        _pre_export = 'pip uninstall -y kamaki || true\n'
-        _pre_export += 'pip uninstall -y kamaki || true'
+        _pre_export = 'pip uninstall -y kamaki || true'
+        _pre_export += '\npip uninstall -y kamaki || true'
+        _pre_export += '\nssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa < /dev/null || true'
         return super(OkeanosClientCloud, self)._get_bootstrap_script(
             node_instance, pre_export=_pre_export, pre_bootstrap=pre_bootstrap,
             post_bootstrap=post_bootstrap, username=username)
